c80ea6645508f019c2281dafb7c14c6a2b650900
Logan Collins <logan@lcspace.net>
2019-12-02 18:30:19 -0600

    move root under 2019 so I only need one repo

--- 2019/rust/src/advent/day01.rs
+++ 2019/rust/src/advent/day01.rs
@@ -5,7 +5,7 @@ fn fuel_cost(weight: f32) -> u32 {
     ((weight / 3.0) as u32).checked_sub(2).unwrap_or(0)
 }
 
-fn recursive_cost(weight: f32) -> u32 {
+fn iterative_cost(weight: f32) -> u32 {
     let mut cost = fuel_cost(weight);
     let mut additional_cost = fuel_cost(cost as f32);
     while additional_cost > 0 {
@@ -17,14 +17,14 @@ fn recursive_cost(weight: f32) -> u32 {
 
 pub fn part_one() -> Result<u32, Error> {
     let input_path = problem_input_path(1, None);
-    let numbers = read_file_split_whitespace(&input_path);
-    let sum = numbers?.into_iter().map(fuel_cost).sum();
+    let numbers = read_file_split_whitespace(&input_path)?;
+    let sum = numbers.into_iter().map(fuel_cost).sum();
     Ok(sum)
 }
 
 pub fn part_two() -> Result<u32, Error> {
     let input_path = problem_input_path(1, None);
-    let numbers = read_file_split_whitespace(&input_path);
-    let sum = numbers?.into_iter().map(recursive_cost).sum();
+    let numbers = read_file_split_whitespace(&input_path)?;
+    let sum = numbers.into_iter().map(iterative_cost).sum();
     Ok(sum)
 }
